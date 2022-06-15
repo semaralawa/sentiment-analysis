5200c32b3907859e5bcdb71c9ece77fdb52e6605
dewasanta2 <dewasanta2@gmail.com>
2022-06-15 15:30:41 +0800

    update front-end design

--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName("toggler-navbar")[0].addEventListener("click", toogleClass);
-
-function toogleClass() {
-    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open')
-    document.getElementsByClassName("sidebar")[0].classList.toggle('open')
-}
