b51bfa643d78cccf510fc8ad5ed034e7c01b6814
dewasanta2 <dewasanta2@gmail.com>
2022-07-10 16:55:14 +0800

    edit home route to dashboard edit result route, now sync with mongoDB add delete funcion to delete data

--- home.py
+++ home.py
@@ -1,45 +1,49 @@
-# import functools
-from predict import predict
-import getData
-
 from flask import (
-    Blueprint, render_template, request
-)
+    Blueprint, redirect, render_template, url_for, session)
+from app import db
+import json
+from bson import json_util
+from bson.objectid import ObjectId
 
 # create blueprint
 bp = Blueprint('home', __name__, url_prefix='/')
 
 
-@bp.route('/home', methods=('GET', 'POST'))
-def home():
-    return render_template('dashboard.html')
+@bp.route('/dashboard', methods=('GET', 'POST'))
+def dashboard():
+    hist_cursor = db.history.find()
+    hist_data = []
+    for data in hist_cursor:
+        hist_data.append(data)
+    print(session)
+    return render_template('dashboard.html', hist_data=hist_data)
 
 
-@bp.route('/result', methods=('GET', 'POST'))
-def result():
-    if request.method == 'POST':
-        print(request.form)
-        key = request.form['keyword']
-        if (request.form.get('twitter')):
-            print('anjay')
-
-        # get data from every sources
-        tweets = getData.twitter(key)
-
-        # predict tweet
-        predict_result = []
-        for tweet in tweets['data']:
-            # print(tweet)
-            result = predict(tweet['text'])
-            tweet.update(result)
-            print(tweet)
-            predict_result.append(tweet)
-
-        # print(predict_result)
-
-        return render_template('tables.html', datas=predict_result)
+@bp.route('/result/<data_id>', methods=('GET', 'POST'))
+def result(data_id):
+    hist_data = db.history.find_one({'_id': ObjectId(data_id)})
+    result_cursor = db.result.find({'hist_id': ObjectId(data_id)})
+    result_data = []
+    for data in result_cursor:
+        result_data.append(data)
+    return render_template('tables copy.html', hist_data=json.loads(json_util.dumps(hist_data)),  # noqa:E501
+                           result_data=result_data)
 
 
 @bp.route('/test', methods=('GET', 'POST'))
 def test():
-    return render_template('tables.html')
+    return render_template('test.html')
+
+
+@bp.route('/delete/history/<data_id>', methods=('GET', 'POST'))
+def deleteHistory(data_id):
+    try:
+        db['history'].delete_one({'_id': ObjectId(data_id)})
+        result = db['result'].delete_many({'hist_id': ObjectId(data_id)})
+
+        print(
+            f'successfully delete history with id {data_id} and {result.deleted_count} result')  # noqa: E501
+    except Exception as err:
+        print(err)
+
+    return redirect(url_for('home.dashboard'))
